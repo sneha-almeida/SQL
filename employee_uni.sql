bd06de4361ca9e0847166ddb64147e84141c60e1
sneha-almeida <sneha.almeida.111@gmail.com>
2026-07-12 13:33:44 +0200

    New SQL Problems Solved

---
+++ employee_uni.sql
@@ -0,0 +1,23 @@
+Create table If Not Exists Employees (id int, name varchar(20))
+Create table If Not Exists EmployeeUNI (id int, unique_id int)
+Truncate table Employees
+insert into Employees (id, name) values ('1', 'Alice')
+insert into Employees (id, name) values ('7', 'Bob')
+insert into Employees (id, name) values ('11', 'Meir')
+insert into Employees (id, name) values ('90', 'Winston')
+insert into Employees (id, name) values ('3', 'Jonathan')
+Truncate table EmployeeUNI
+insert into EmployeeUNI (id, unique_id) values ('3', '1')
+insert into EmployeeUNI (id, unique_id) values ('11', '2')
+insert into EmployeeUNI (id, unique_id) values ('90', '3')
+
+# Write your MySQL query statement below
+select EmployeeUNI.unique_id, Employees.name
+from Employees LEFT JOIN EmployeeUNI
+ON Employees.id = EmployeeUNI.id
+
+# Syntax for Right Join 
+#Select *
+#from Table 1 
+#Right Join table 2
+#on condition
---
+++ products.sql
@@ -0,0 +1,14 @@
+Create table If Not Exists Sales (sale_id int, product_id int, year int, quantity int, price int)
+Create table If Not Exists Product (product_id int, product_name varchar(10))
+Truncate table Sales
+insert into Sales (sale_id, product_id, year, quantity, price) values ('1', '100', '2008', '10', '5000')
+insert into Sales (sale_id, product_id, year, quantity, price) values ('2', '100', '2009', '12', '5000')
+insert into Sales (sale_id, product_id, year, quantity, price) values ('7', '200', '2011', '15', '9000')
+Truncate table Product
+insert into Product (product_id, product_name) values ('100', 'Nokia')
+insert into Product (product_id, product_name) values ('200', 'Apple')
+insert into Product (product_id, product_name) values ('300', 'Samsung')
+# Write your MySQL query statement below
+select s.year, s.price, p.product_name 
+from Sales as s, Product as p 
+where s.product_id = p.product_id
